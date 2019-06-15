6ebb4808bdda70bb939770aab8703b6359a7747a
Varvara Bazhukova <mamasetta@gmail.com>
2019-06-15 18:09:52 +0500

    Еще js и анимация

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,24 @@
 var searchButton = document.querySelector(".search__button");
 var searchForm = document.querySelector(".search__form");
+var searchDateIn = document.querySelector(".search-form__date--in");
+var searchDateOut = document.querySelector(".search-form__date--out");
 
 searchForm.classList.add('hidden');
 
 searchButton.addEventListener("click", function(event) {
   event.preventDefault();
   searchForm.classList.toggle('hidden');
+  searchForm.classList.toggle('opening');
+});
+
+searchForm.addEventListener("submit", function (event) {
+  if (!searchDateIn.value || !searchDateOut.value) {
+    event.preventDefault();
+    searchButton.addEventListener("click", function(event) {
+      event.preventDefault();
+    });
+    searchDateIn.classList.add("error");
+    searchDateOut.classList.add("error");
+    alert("Укажите дату заезда и дату выезда");
+  }
 });
